b6a7dc099ff051e0b81895444d684e004a6451f6
whutxd <whutxd@163.com>
2025-06-06 16:34:47 +0800

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 Author: whutxd whutxd@163.com
 Date: 2025-06-05 17:22:44
 LastEditors: whutxd whutxd@163.com
-LastEditTime: 2025-06-05 17:46:16
+LastEditTime: 2025-06-06 16:33:34
 FilePath: \新建文件夹\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -23,4 +23,5 @@ if __name__ == '__main__':
 
 
 # 设定
+# 123
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
