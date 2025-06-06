bc59ff3f426aefd14091a4a32922a2cc4d16aeb8
whutxd <whutxd@163.com>
2025-06-06 16:30:05 +0800

    first commit: 添加功能1

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+Author: whutxd whutxd@163.com
+Date: 2025-06-05 17:22:44
+LastEditors: whutxd whutxd@163.com
+LastEditTime: 2025-06-05 17:46:16
+FilePath: \新建文件夹\main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+# 设定
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
